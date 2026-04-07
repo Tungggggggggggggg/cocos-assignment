06dabe07ec8e147dd18369de61430b3bf9b3a3fb
Nguyễn Quốc Tùng <110363807+Tungggggggggggggg@users.noreply.github.com>
2026-04-07 17:10:02 +0700

    xoá

--- 2-4-2026/ex2/bai3.js
+++
@@ -1,8 +0,0 @@
-function randomElement(arr) {
-  if (!arr || arr.length === 0) return undefined;
-
-  return arr[randomInt(0, arr.length - 1)];
-}
-
-console.log(randomElement([10, 20, 30, 40]));
-console.log(randomElement([]));
--- 2-4-2026/ex2/bai4.js
+++
@@ -1,9 +0,0 @@
-function findMissingElements(arr1, arr2) {
-  const set1 = new Set(arr1);
-
-  return arr2.filter((item) => !set1.has(item));
-}
-
-console.log(findMissingElements([1, 2, 3], [1, 2, 3, 4, 5]));
-console.log(findMissingElements([1, 3, 5], [1, 2, 3, 4, 5]));
-console.log(findMissingElements([], [1, 2, 3]));
